455177c8a5bfb26c14123ae8d2f9bf6f1ddd828b
Samuel Ocran <sammiestt@gmail.com>
2022-02-20 09:26:58 +0100

    chore(database): setup postgres database with sequelize

---
+++ src/database/migrations/20220220080745-create-contact.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Contacts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      phoneNumber: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Contacts');
+  }
+};
---
+++ src/database/seeders/20220220081726-contact.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Contacts', [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john_doe@gmail.com',
+        phoneNumber: '+2348093833748',
+      },
+      {
+        firstName: 'Jane',
+        lastName: 'Doe',
+        email: 'jane_doe@gmail.com',
+        phoneNumber: '+2347033889900',
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('Contacts', null, {});
+     */
+  }
+};
